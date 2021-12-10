f50fdb0a8ae6c6cf14923eeee38ac1d567f359f2
euanleith <51672571+euanleith@users.noreply.github.com>
2021-12-10 17:44:45 +0000

    bungus 1923

--- main.py
+++ main.py
@@ -6,25 +6,46 @@ import requests
 import numpy as np
 import pickle
 
-DISCORD_TOKEN = '<token>' # find here: https://discord.com/developers/applications/783103691036557362/bot
+DISCORD_TOKEN = 'NzgzMTAzNjkxMDM2NTU3MzYy.X8V4JQ.e_KeGOfJreqGtWVSEziPFKCIS5w'
 TENOR_TOKEN = '6RBF86B3QSNB'
 GIFS = np.array(['floppa', 'sogga', 'venti', 'thoma', 'diluc', 'amongus', 'femboy', 'malewife', 'doge', 'frog',
-                 'peepo', 'pepe', 'horny jail', 'crazy frog'])
+                 'peepo', 'pepe', 'horny jail', 'crazy frog', 'googas', 'nigella', 'plinka'])
 participants = pickle.load(open('tuple.dump', 'rb'))
 wishlists = pickle.load(open('wishlists.dump', 'rb'))
+chungus = pickle.load(open('chungus.dump', 'rb'))  # who got who
 
 client = discord.Client()
 
+# kitty 536674156150784000 - ella
+# anna 243136602170195968 - kitty
+# euan 486932576330711040 - anna
+# ger 200999737405603840 - jason
+# evan - ger
+# ella 290479359293128704 - ?
+# jason - euan
+# milly - ?
 
 def assign(participants):
     while True:
         rand = random.sample(participants, len(participants))
         cont = False
         for r, p in zip(rand, participants):
-            if r == p:
+            if r == p or \
+                    (p[0] == 536674156150784000 and r[0] == 290479359293128704) or \
+                    (p[0] == 243136602170195968 and r[0] == 536674156150784000) or \
+                    (p[0] == 486932576330711040 and r[0] == 243136602170195968) or \
+                    (p[0] == 200999737405603840 and r[0] == 123) or \
+                    (p[0] == 123 and r[0] == 200999737405603840) or \
+                    (p[0] == 290479359293128704 and r[0] == 123) or \
+                    (p[0] == 123 and r[0] == 486932576330711040) or \
+                    (p[0] == 123 and r[0] == 123):
                 cont = True
                 break
         if not cont:
+            chungus.clear()
+            for r, p in zip(rand, participants):
+                chungus[p] = r
+            pickle.dump(chungus, open('chungus.dump', 'wb'))
             return rand
 
 
@@ -81,12 +102,17 @@ async def on_message(message):
                 out = 'very cool, unfortunately i have had intercourse with your mother'
                 pickle.dump(participants, open('tuple.dump', 'wb'))
             elif content.startswith('kick'):
-                if content[5:] in wishlists:
-                    [participants.remove(p) for p in participants if p[1] == content[5:]]
-                    del wishlists[content[5:]]
-                    out = 'the daemon ' + content[5:] + ' has been excised'
+                if message.author.id == 486932576330711040:
+                    if content[5:] in wishlists:
+                        [participants.remove(p) for p in participants if p[1] == content[5:]]
+                        del wishlists[content[5:]]
+                        pickle.dump(participants, open('tuple.dump', 'wb'))
+                        pickle.dump(wishlists, open('wishlists.dump', 'wb'))
+                        out = 'the daemon ' + content[5:] + ' has been excised'
+                    else:
+                        out = 'who tf is ' + content[5:] + ' frfr'
                 else:
-                    out = 'who tf is ' + content[5:] + ' frfr'
+                    out = 'bad boy'
             elif content == 'run':
                 if len(participants) < 2:
                     out = 'get more friends'
@@ -107,33 +133,34 @@ async def on_message(message):
                 if not author[1] in wishlists:
                     out = 'you do not exist'
                 else:
-                    # todo if run has been done, user should be able to easily find the wishlist of the person they got without inputting their name
                     msg = content[8:]
-                    if msg == '':
-                        if len(wishlists[author[1]]) == 0:
-                            out = 'ERROR MUNGUS NOT FOUND'
-                        else:
-                            out = wishlists[author[1]]
+                    if msg == '' or msg == 's':
+                        out = ''
+                        for k in wishlists:
+                            out += k + ': ' + str(wishlists[k]) + '\n'
                     elif msg.startswith(' add'):
-                        wishlists[author[1]].append(content[14:])
-                        out = "added " + content[14:]
+                        wishlists[author[1]].append(content[13:])
+                        out = "added " + content[13:]
                     elif msg.startswith(' remove'):
-                        if content[17:] not in wishlists[author[1]]:
-                            out = "didn't find " + content[17:]
+                        if content[16:] not in wishlists[author[1]]:
+                            out = "didn't find " + content[16:]
                         else:
-                            wishlists[author[1]].remove(content[17:])
-                            out = "removed " + content[17:]
+                            wishlists[author[1]].remove(content[16:])
+                            out = "removed " + content[16:]
                     elif msg == ' clear':
                         wishlists[author[1]] = list()
                         out = 'the deed has been done'
                     elif msg[1:] in wishlists:
-                        if len(wishlists[msg[1:]]) == 0:
-                            out = 'ERROR MUNGUS NOT FOUND'
-                        else:
-                            out = wishlists[msg[1:]]
+                        out = wishlists[msg[1:]]
                     else:
                         out = 'who tf is' + msg + ' frfr'
                     pickle.dump(wishlists, open('wishlists.dump', 'wb'))
+            elif content == 'clear chungus':
+                chungus.clear()
+                pickle.dump(chungus, open('chungus.dump', 'wb'))
+                out = 'CHUNGUS CLEARED'
+            elif content == 'ids':
+                out = participants
             else:
                 await message.channel.send('wot tf u on about matey alri ya')
             await message.channel.send(str(out) + ' ' + str(randEmoji(client)))
